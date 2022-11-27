b6ac68f02204814c67d4c0b5c52d9e5c5d37c733
Patricia <pato.rgutierrez@gmail.com>
2022-11-26 22:51:13 -0500

    actualizacion pagos

--- src/Factura.jsx
+++ src/Factura.jsx
@@ -1,64 +1,65 @@
 import "./Estilos.css"
-function Factura(){
-    return (<div className="Factura">
+function Factura() {
+     return (<div className="Contenido">
 
-     
-     <div className="ContenidoFactura">
-        <div>
-                              
-     <p>
-        <h1>
-        Factura 
-        </h1>
-        </p>   
-        
-              
-        <table >
-               
-                 <tr>                 
-                 <th>SERVICIO</th>
-                 <th colspan="10">VALOR</th>
-                </tr>
-                <tr>
-                <td>Lorem ipsum</td>  
-                <td>000.000</td>
-                <tr>
-                
-                </tr>
-                                  
-                 <td>Lorem ipsum</td>
-                 <td>000.000</td>
-                 </tr>
-                 <tr>                               
-                                  
-                 <td>000.000</td>                
-                 <td>Total a pagar </td>              
-                 
-                 </tr>   
-                    
-                          </table>
-               
-            <div>
 
-             <div className="botones">
-             <button id="Generar">
-                 Generar
-             </button>
+          <div>
+               <div className="servicio">
 
-             <button id="PSE">
-                  PSE
-                  </button>
-                  <button>
-             <button id="Tarjetas Credito" >
-                  Tarjetas Credito
-             </button>
-             </button>
-             </div>
-             </div>
-             </div>
-             </div>
-             </div>
-            )                                
+                    <p>
+                         <h2>
+                              Factura
+                         </h2>
+                    </p>
+
+
+                    <table >
+
+                         <tr>
+                              <th>SERVICIO</th>
+                              <th colspan="10">VALOR</th>
+                         </tr>
+                         <tr>
+                              <td>Lorem ipsum</td>
+                              <td>000.000</td>
+                              <tr>
+
+                              </tr>
+
+                              <td>Lorem ipsum</td>
+                              <td>000.000</td>
+                         </tr>
+                         <tr>
+
+                              <td>000.000</td>
+                              <td>Total a pagar </td>
+
+                         </tr>
+                    </table>
+
+                    <div>
+
+                         <div className="botones">
+                              <button id="Generar">
+                                   Generar
+                              </button>
+
+                              <button id="Pagar2">
+                                   Pagar
+                              </button>  
+                         </div>
+                    </div>
+               </div>
+          </div>
+          <div className="botones">
+               <a id='pse' href="https://clinicapse.s3.amazonaws.com/index.html
+">PSE</a>
+               <a id='tarjeta' href="https://clinicatarjeta.s3.amazonaws.com/index.html
+npm run dev
+">Tarjeta Crédito</a>
+          </div>
+     </div>
+     )
 }
-               
+
 export default Factura
--- src/Pagos.jsx
+++ src/Pagos.jsx
@@ -1,52 +1,52 @@
 import "./Estilos.css"
 
-function Pagos(){
+function Pagos() {
     return (<div className="Contenido">
 
-     
-<div className="servicio">
-     <p>
-        <h1>
-        SERVICIOS POR PAGAR
-        </h1>
-    </p>
-    <p>
-    Paciente: Lorem ipsum dolor sit amet       
-    </p>
-    <p>
-    Documento: 0000000000
-    </p>
-    <p>
-    Prestación servicio: Lorem ipsum
-    </p>
-    <p>
-    Fecha: 01/01/1900
-    </p>
-    <p>
-    Hora: 00:00
-    </p>
-    <p>
-    Consultorio: 000
-    </p>
-    <p>
-    Profesional: Nombres Apellidos
-    </p>
-    <p>
-    Tipo Servicio: Lorem ipsum dolor
-    </p>
-    <p>
-    Copago: 0.000
-    </p>
-    <div className="botones">
-    <button id="Agregar">
-        Agregar
-    </button>
-    <button id="Pagar" >
-         Pagar
-    </button>
-    </div>
-    
-</div>
+
+        <div className="servicio">
+            <p>
+                <h1>
+                    SERVICIOS POR PAGAR
+                </h1>
+            </p>
+            <p>
+                Paciente: Lorem ipsum dolor sit amet
+            </p>
+            <p>
+                Documento: 0000000000
+            </p>
+            <p>
+                Prestación servicio: Lorem ipsum
+            </p>
+            <p>
+                Fecha: 01/01/1900
+            </p>
+            <p>
+                Hora: 00:00
+            </p>
+            <p>
+                Consultorio: 000
+            </p>
+            <p>
+                Profesional: Nombres Apellidos
+            </p>
+            <p>
+                Tipo Servicio: Lorem ipsum dolor
+            </p>
+            <p>
+                Copago: 0.000
+            </p>
+            <div className="botones">
+                <button id="Agregar">
+                    Agregar
+                </button>
+                <button id="Pagar" >
+                    Pagar
+                </button>
+            </div>
+
+        </div>
 
 
     </div>)
@@ -54,4 +54,3 @@ function Pagos(){
 }
 
 export default Pagos;
-
